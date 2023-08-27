46098fe2ecf3505a880048c4941a349911c81ebe
Jhotech <nduguzoj@gmail.com>
2023-08-27 00:18:24 -0700

    Added slider

--- src/component/BuyTicket.js
+++ src/component/BuyTicket.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
+import {Slider, Stack} from "@mui/material";
+import {VolumeDown, VolumeUp} from "@mui/icons-material";
 
 export default function BuyTicket() {
     return (
@@ -16,6 +18,13 @@ export default function BuyTicket() {
             <TextField id="outlined-basic" label="Outlined" variant="outlined" />
             <TextField id="filled-basic" label="Filled" variant="filled" />
             <TextField id="standard-basic" label="Standard" variant="standard" />
+
+            <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+                <VolumeDown />
+                <Slider aria-label="Volume" value={value} />
+                <VolumeUp />
+            </Stack>
+            <Slider disabled defaultValue={30} aria-label="Disabled slider" />
 
             <Button variant="contained">Contained</Button>
         </Box>
